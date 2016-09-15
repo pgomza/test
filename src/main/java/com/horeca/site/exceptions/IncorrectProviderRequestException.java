998ef3e4753af46fb87f089a97d50bb6479934dd
Wojciech Pomykała <wojt.pomykal@gmail.com>
2016-09-15 02:38:49 +0200

    Get rid of unused validation-related classes

--- src/main/java/com/horeca/site/exceptions/IncorrectProviderRequestException.java
+++
@@ -1,8 +0,0 @@
-package com.horeca.site.exceptions;
-
-public class IncorrectProviderRequestException extends RuntimeException {
-
-    public IncorrectProviderRequestException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/horeca/site/exceptions/UnsupportedSocialProviderException.java
+++
@@ -1,8 +0,0 @@
-package com.horeca.site.exceptions;
-
-public class UnsupportedSocialProviderException extends RuntimeException {
-
-    public UnsupportedSocialProviderException(String message) {
-        super(message);
-    }
-}
