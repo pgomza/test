1ddfe10323ad6e9a57d530bd169b8e18e6ea1f6a
Wojciech Pomykała <wojt.pomykal@gmail.com>
2016-09-15 02:31:40 +0200

    Modify StayController to make use of StayService

--- src/main/java/com/horeca/site/controllers/StayController.java
+++ src/main/java/com/horeca/site/controllers/StayController.java
@@ -1,66 +1,56 @@
 package com.horeca.site.controllers;
 
 import com.horeca.site.models.Stay;
-import com.horeca.site.repositories.StayRepository;
-import com.horeca.site.services.StayRegistrationService;
-import com.horeca.site.validation.ResourceNotFoundException;
+import com.horeca.site.services.StayService;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @Api(value = "stays")
 @RestController
-@RequestMapping("/api/stays")
+@RequestMapping("/api")
 public class StayController {
 
     @Autowired
-    private StayRepository repository;
+    private StayService stayService;
 
-    @Autowired
-    private StayRegistrationService registrationService;
-
-    @RequestMapping(value = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/stays", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Iterable<Stay> getAll() {
-        return repository.findAll();
+        return stayService.getAll();
     }
 
-    @RequestMapping(value = "/{pin}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    @PreAuthorize("authentication.userAuthentication.details['pin'] == #pin")
-    public Stay get(@PathVariable String pin) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        checkIfExists(pin);
-        return repository.findOne(pin);
-    }
-
-    @RequestMapping(value = "", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/stays", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public Stay add(@Valid @RequestBody Stay entity) {
-        return registrationService.registerNewStay(entity);
+        return stayService.registerNewStay(entity);
     }
 
-    @RequestMapping(value = "/{pin}", method = RequestMethod.PUT)
-    @PreAuthorize("authentication.userAuthentication.details['pin'] == #pin")
-    public Stay update(@Valid @RequestBody Stay entity, @PathVariable String pin) {
-        checkIfExists(pin);
-        entity.setPin(pin);
-        return repository.save(entity);
+    @RequestMapping(value = "/stays/{pin}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Stay get(@PathVariable String pin) {
+        return stayService.get(pin);
     }
 
-    @RequestMapping(value = "/{pin}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
-    @PreAuthorize("authentication.userAuthentication.details['pin'] == #pin")
+    @RequestMapping(value = "/stays/{pin}", method = RequestMethod.PUT)
+    public Stay update(@PathVariable String pin, @Valid @RequestBody Stay entity) {
+        return stayService.update(pin, entity);
+    }
+
+    @RequestMapping(value = "/stays/{pin}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
     public void delete(@PathVariable String pin) {
-        checkIfExists(pin);
-        repository.delete(pin);
+        stayService.delete(pin);
     }
 
-    private void checkIfExists(String pin) {
-        boolean exists = repository.exists(pin);
-        if (!exists)
-            throw new ResourceNotFoundException("Such Stay does not exist");
+    @RequestMapping(value = "/check-in/{pin}", method = RequestMethod.POST,
+            consumes = MediaType.ALL_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Stay checkIn(@PathVariable String pin) {
+        return stayService.checkIn(pin);
+    }
+
+    @RequestMapping(value = "/check-out/{pin}", method = RequestMethod.POST,
+            consumes = MediaType.ALL_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public void checkOut(@PathVariable String pin) {
+        stayService.checkOut(pin);
     }
 }
