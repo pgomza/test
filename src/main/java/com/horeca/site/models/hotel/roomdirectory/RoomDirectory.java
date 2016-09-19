7522fc5f62b48e8ae103e45758df4d280d72ae12
Wojciech Pomykała <wojt.pomykal@gmail.com>
2016-09-19 04:29:43 +0200

    Make use of Translation and Viewable types in RoomDirectory-related models

--- src/main/java/com/horeca/site/models/hotel/roomdirectory/RoomDirectory.java
+++ src/main/java/com/horeca/site/models/hotel/roomdirectory/RoomDirectory.java
@@ -1,19 +1,22 @@
 package com.horeca.site.models.hotel.roomdirectory;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.horeca.site.models.Viewable;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 @Entity
-public class RoomDirectory {
+public class RoomDirectory implements Viewable<RoomDirectoryView> {
 
     @Id
     @GeneratedValue
     @JsonIgnore
     private Long id;
 
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinColumn
     private Set<Section> sections;
 
@@ -31,5 +34,19 @@ public class RoomDirectory {
 
     public void setSections(Set<Section> sections) {
         this.sections = sections;
+    }
+
+    @Override
+    public RoomDirectoryView toView(String preferredLanguage, String defaultLanguage) {
+        Set<Section> sections = getSections();
+        RoomDirectoryView roomDirectoryView = new RoomDirectoryView();
+
+        List<SectionView> sectionViews = new ArrayList<>();
+        for (Section section : sections) {
+            sectionViews.add(section.toView(preferredLanguage, defaultLanguage));
+        }
+
+        roomDirectoryView.setSections(sectionViews);
+        return roomDirectoryView;
     }
 }
--- src/main/java/com/horeca/site/models/hotel/roomdirectory/Section.java
+++ src/main/java/com/horeca/site/models/hotel/roomdirectory/Section.java
@@ -2,6 +2,7 @@ package com.horeca.site.models.hotel.roomdirectory;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.horeca.site.models.Translatable;
+import com.horeca.site.models.Viewable;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -10,7 +11,7 @@ import java.util.List;
 import java.util.Set;
 
 @Entity
-public class Section extends Translatable<SectionTranslation> {
+public class Section extends Translatable<SectionTranslation> implements Viewable<SectionView> {
 
     @Id
     @GeneratedValue
@@ -23,5 +24,15 @@ public class Section extends Translatable<SectionTranslation> {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    @Override
+    public SectionView toView(String preferredLanguage, String defaultLanguage) {
+        SectionView sectionView = new SectionView();
+        SectionTranslation translation = getTranslation(preferredLanguage, defaultLanguage);
+        sectionView.setHeading(translation.getHeading());
+        sectionView.setText(translation.getText());
+
+        return sectionView;
     }
 }
