c50d9563a85b5b12a6ebf272f8ecce7e0bbdcd41
Wojciech Pomykała <wojt.pomykal@gmail.com>
2016-10-03 14:54:36 +0200

    Add CarPark controller and service

---
+++ src/main/java/com/horeca/site/controllers/services/CarParkController.java
@@ -0,0 +1,29 @@
+package com.horeca.site.controllers.services;
+
+import com.horeca.site.models.hotel.services.carpark.CarParkView;
+import com.horeca.site.models.hotel.services.spa.SpaView;
+import com.horeca.site.services.CarParkService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Api(value = "hotels")
+@RestController
+@RequestMapping("/api/hotels")
+public class CarParkController {
+
+    @Autowired
+    private CarParkService carParkService;
+
+    @RequestMapping(value = "/{hotelId}/services/carpark", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public CarParkView get(@PathVariable("hotelId") Long hotelId, HttpServletRequest request) {
+        String language = request.getLocale().getLanguage();
+        return carParkService.getView(hotelId, language);
+    }
+}
---
+++ src/main/java/com/horeca/site/services/CarParkService.java
@@ -0,0 +1,22 @@
+package com.horeca.site.services;
+
+import com.horeca.site.models.hotel.Hotel;
+import com.horeca.site.models.hotel.services.carpark.CarPark;
+import com.horeca.site.models.hotel.services.carpark.CarParkView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class CarParkService {
+
+    @Autowired
+    private HotelService hotelService;
+
+    public CarParkView getView(Long hotelId, String preferredLanguage) {
+        Hotel hotel = hotelService.get(hotelId);
+        CarPark carPark = hotel.getAvailableServices().getCarPark();
+        return carPark.toView(preferredLanguage, hotel.getDefaultTranslation());
+    }
+}
