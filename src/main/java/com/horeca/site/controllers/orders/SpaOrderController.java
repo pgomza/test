2f7d77bb3e525c1cebc46b81dd73fddf1c15f9f0
Wojciech Pomykała <wojt.pomykal@gmail.com>
2016-10-05 00:55:15 +0200

    Add spa order controller; limitations dictated by current service's implementation

---
+++ src/main/java/com/horeca/site/controllers/orders/SpaOrderController.java
@@ -0,0 +1,44 @@
+package com.horeca.site.controllers.orders;
+
+import com.horeca.site.models.orders.OrderStatusPUT;
+import com.horeca.site.models.orders.spa.SpaOrder;
+import com.horeca.site.models.orders.spa.SpaOrderPOST;
+import com.horeca.site.models.orders.spa.SpaOrderView;
+import com.horeca.site.services.SpaOrderService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+@Api(value = "orders")
+@RestController
+@RequestMapping("/api/stays")
+public class SpaOrderController {
+
+    @Autowired
+    private SpaOrderService service;
+
+    @RequestMapping(value = "/{pin}/orders/spa", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public SpaOrderView get(@PathVariable String pin, HttpServletRequest request) {
+        String preferredLanguage = request.getLocale().getLanguage();
+        return service.getActiveView(pin, preferredLanguage);
+    }
+
+    @RequestMapping(value = "/{pin}/orders/spa", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public SpaOrder add(@PathVariable String pin, @Valid @RequestBody SpaOrderPOST newOrder) {
+        return service.add(pin, newOrder);
+    }
+
+    @RequestMapping(value = "/{pin}/orders/spa/status", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public OrderStatusPUT getStatus(@PathVariable String pin) {
+        return service.getActiveStatus(pin);
+    }
+
+    @RequestMapping(value = "/{pin}/orders/spa/status", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    public OrderStatusPUT updateStatus(@PathVariable String pin, @Valid @RequestBody OrderStatusPUT newStatus) {
+        return service.updateActiveStatus(pin, newStatus);
+    }
+}
