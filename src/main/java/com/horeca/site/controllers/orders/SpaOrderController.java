c334bb55fb0bcd8bb3fd65f5a6142ae6370c522b
Wojciech Pomykała <wojt.pomykal@gmail.com>
2016-10-11 19:49:15 +0200

    Update SpaOrderController to make use of updated service

--- src/main/java/com/horeca/site/controllers/orders/SpaOrderController.java
+++ src/main/java/com/horeca/site/controllers/orders/SpaOrderController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.util.Set;
 
 @Api(value = "orders")
 @RestController
@@ -22,9 +23,15 @@ public class SpaOrderController {
     private SpaOrderService service;
 
     @RequestMapping(value = "/{pin}/orders/spa", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public SpaOrderView get(@PathVariable String pin, HttpServletRequest request) {
+    public Set<SpaOrderView> getAll(@PathVariable String pin, HttpServletRequest request) {
         String preferredLanguage = request.getLocale().getLanguage();
-        return service.getActiveView(pin, preferredLanguage);
+        return service.getAllViews(pin, preferredLanguage);
+    }
+
+    @RequestMapping(value = "/{pin}/orders/spa/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public SpaOrderView get(@PathVariable String pin, @PathVariable Long id, HttpServletRequest request) {
+        String preferredLanguage = request.getLocale().getLanguage();
+        return service.getView(pin, id, preferredLanguage);
     }
 
     @RequestMapping(value = "/{pin}/orders/spa", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -32,13 +39,18 @@ public class SpaOrderController {
         return service.add(pin, newOrder);
     }
 
-    @RequestMapping(value = "/{pin}/orders/spa/status", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public OrderStatusPUT getStatus(@PathVariable String pin) {
-        return service.getActiveStatus(pin);
+    @RequestMapping(value = "/{pin}/orders/spa/{id}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    public SpaOrder update(@PathVariable String pin, @PathVariable Long id, @Valid @RequestBody SpaOrder updated) {
+        return service.update(pin, id, updated);
     }
 
-    @RequestMapping(value = "/{pin}/orders/spa/status", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
-    public OrderStatusPUT updateStatus(@PathVariable String pin, @Valid @RequestBody OrderStatusPUT newStatus) {
-        return service.updateActiveStatus(pin, newStatus);
+    @RequestMapping(value = "/{pin}/orders/spa/{id}/status", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public OrderStatusPUT getStatus(@PathVariable String pin, @PathVariable Long id) {
+        return service.getStatus(pin, id);
+    }
+
+    @RequestMapping(value = "/{pin}/orders/spa/{id}/status", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    public OrderStatusPUT updateStatus(@PathVariable String pin, @PathVariable Long id, @Valid @RequestBody OrderStatusPUT newStatus) {
+        return service.updateStatus(pin, id, newStatus);
     }
 }
