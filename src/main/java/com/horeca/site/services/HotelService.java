393e627b4a4a341507416555d2c02a4fb21a24fe
Wojciech Pomykała <wojt.pomykal@gmail.com>
2016-12-11 02:30:54 +0100

    Add hardcoded panel client at startup

--- src/main/java/com/horeca/site/services/HotelService.java
+++ src/main/java/com/horeca/site/services/HotelService.java
@@ -1,17 +1,41 @@
 package com.horeca.site.services;
 
+import com.google.common.hash.Hashing;
 import com.horeca.site.exceptions.ResourceNotFoundException;
 import com.horeca.site.models.hotel.Hotel;
+import com.horeca.site.models.user.UserInfo;
 import com.horeca.site.repositories.HotelRepository;
+import com.horeca.site.security.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.PostConstruct;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class HotelService {
 
     @Autowired
     private HotelRepository repository;
+
+    @Autowired
+    private LoginService loginService;
+
+    @PostConstruct
+    private void addHardcodedPanelClient() {
+        String hardcodedUsername = "admin";
+        if (!loginService.isAlreadyPresent(hardcodedUsername)) {
+            List<String> roles = new ArrayList<>(Arrays.asList("ROLE_HOTEL_1"));
+            String hardcodedPassword = Hashing.sha256().hashString("throdi", StandardCharsets.UTF_8).toString();
+            UserInfo userInfo = new UserInfo(UserInfo.AUTH_PREFIX_LOGIN + hardcodedUsername, hardcodedPassword, roles);
+            loginService.saveUser(userInfo);
+        }
+    }
 
     public Iterable<Hotel> getAll() {
         return repository.findAll();
