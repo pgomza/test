c54cdfa1a413be92f85dbfb734677598a9462eec
Wojciech Pomykała <wojt.pomykal@gmail.com>
2017-01-11 23:08:25 +0100

    Add RoomService models

---
+++ src/main/java/com/horeca/site/models/hotel/services/roomservice/RoomService.java
@@ -0,0 +1,59 @@
+package com.horeca.site.models.hotel.services.roomservice;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.horeca.site.models.Price;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Set;
+
+@Entity
+public class RoomService {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
+    private Long id;
+
+    private String description;
+
+    @NotNull
+    @Embedded
+    private Price price;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "room_service_id")
+    private Set<RoomServiceCategory> categories;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Price getPrice() {
+        return price;
+    }
+
+    public void setPrice(Price price) {
+        this.price = price;
+    }
+
+    public Set<RoomServiceCategory> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<RoomServiceCategory> categories) {
+        this.categories = categories;
+    }
+}
