60ec95a35830b22a0d0d10333ebf051321015e49
Wojciech Pomykała <wojt.pomykal@gmail.com>
2017-02-13 04:00:13 +0100

    Add basic version of HotelDataController (only 'get' methods have been implemented)

---
+++ src/main/java/com/horeca/site/controllers/HotelDataController.java
@@ -0,0 +1,32 @@
+package com.horeca.site.controllers;
+
+import com.horeca.site.models.hoteldata.HotelData;
+import com.horeca.site.services.HotelDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/hoteldata")
+public class HotelDataController {
+
+    @Autowired
+    private HotelDataService service;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public HotelData get(@PathVariable("id") Long id) {
+        return service.get(id);
+    }
+
+    @RequestMapping(value = "/search", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public HotelData getByName(@RequestParam(value = "name", required = false) String name) {
+        return service.getByName(name);
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Page<HotelData> getAll(Pageable pageable) {
+        return service.getBatch(pageable);
+    }
+}
