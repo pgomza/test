90b09bc8a1e60fbb877be96403f40827bff32940
Wojciech Pomykała <wojt.pomykal@gmail.com>
2017-02-13 03:55:41 +0100

    Add simple ExtractorController

---
+++ src/main/java/com/horeca/site/controllers/ExtractorController.java
@@ -0,0 +1,24 @@
+package com.horeca.site.controllers;
+
+import com.horeca.site.extractors.HotelDataExtractor;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(value = "stays")
+@RestController
+@RequestMapping("/api")
+public class ExtractorController {
+
+    @Autowired
+    private HotelDataExtractor extractor;
+
+    @RequestMapping(value = "/extractor", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public void extract(@RequestParam(value = "size", required = true) Integer size) {
+        extractor.extract(size);
+    }
+}
