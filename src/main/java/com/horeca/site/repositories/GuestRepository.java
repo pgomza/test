6396f2540c2729e069993bf3fc123acaeb7a0192
Wojciech Pomykała <wojt.pomykal@gmail.com>
2017-03-19 18:51:52 +0100

    Fix auth checks after recent reorganization of hotel guests

--- src/main/java/com/horeca/site/repositories/GuestRepository.java
+++ src/main/java/com/horeca/site/repositories/GuestRepository.java
@@ -1,17 +1,7 @@
 package com.horeca.site.repositories;
 
 import com.horeca.site.models.guest.Guest;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 public interface GuestRepository extends CrudRepository<Guest, Long> {
-
-    @Query("select case when count(g) > 0 then true else false end " +
-            "from Guest g, Stay s where s.guest.id = :guestId and s.hotel.id = :hotelId")
-    boolean checkIfGuestInHotel(@Param("guestId") Long guestId, @Param("hotelId") Long hotelId);
-
-    @Query("select case when count(s) > 0 then true else false end " +
-            "from Stay s where s.guest.id = :guestId")
-    boolean checkIfGuestInAnyStay(@Param("guestId") Long guestId);
 }
--- src/main/java/com/horeca/site/security/AccessChecker.java
+++ src/main/java/com/horeca/site/security/AccessChecker.java
@@ -1,12 +1,13 @@
 package com.horeca.site.security;
 
 import com.horeca.site.models.guest.Guest;
+import com.horeca.site.models.hotel.Hotel;
 import com.horeca.site.models.stay.Stay;
 import com.horeca.site.models.stay.StayPOST;
 import com.horeca.site.models.stay.StayView;
-import com.horeca.site.repositories.GuestRepository;
 import com.horeca.site.security.models.GuestAccount;
 import com.horeca.site.security.models.UserAccount;
+import com.horeca.site.services.HotelService;
 import com.horeca.site.services.services.StayService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -25,13 +27,12 @@ public class AccessChecker {
     private final Pattern stayPinPattern = Pattern.compile("/api/stays/([a-z0-9]+)");
     private final Pattern checkInPattern = Pattern.compile("/api/check-in/([a-z0-9]+)");
     private final Pattern checkOutPattern = Pattern.compile("/api/check-out/([a-z0-9]+)");
-    private final Pattern guestIdPattern = Pattern.compile("/api/guests/(\\d+)");
 
     @Autowired
     private StayService stayService;
 
     @Autowired
-    private GuestRepository guestRepository;
+    private HotelService hotelService;
 
     public boolean checkForHotel(Authentication authentication, HttpServletRequest request) {
         String servletPath = request.getServletPath();
@@ -69,16 +70,6 @@ public class AccessChecker {
         return (allowedToManageHotel && allowedToManageGuest);
     }
 
-    public boolean checkForGuestFromCollection(Authentication authentication, Guest filterObject) {
-        return checkForGuestHelper(authentication, filterObject.getId());
-    }
-
-    public boolean checkForGuest(Authentication authentication, HttpServletRequest request) {
-        String servletPath = request.getServletPath();
-        Long hotelId = extractIdFromServletPath(servletPath, guestIdPattern);
-        return checkForGuestHelper(authentication, hotelId);
-    }
-
     private boolean checkForStayHelper(Authentication authentication, String pin) {
         if (pin != null) {
             Object principal = authentication.getPrincipal();
@@ -99,28 +90,26 @@ public class AccessChecker {
         return false;
     }
 
-    private boolean checkForGuestHelper(Authentication authentication, Long guestId) {
-        Object principal = authentication.getPrincipal();
-        if (principal instanceof UserAccount) {
-            UserAccount userAccount = (UserAccount) principal;
-            boolean guestInTheHotel = guestRepository.checkIfGuestInHotel(guestId, userAccount.getHotelId());
-            if (guestInTheHotel)
-                return true;
-            else {
-                // maybe the currently filtered guest hasn't been associated with any stay yet?
-                // if so, they should be available too
-                return !guestRepository.checkIfGuestInAnyStay(guestId);
-            }
-        }
-        return false;
-    }
-
     private boolean checkForHotelHelper(Authentication authentication, Long hotelId) {
         Object principal = authentication.getPrincipal();
         if (principal instanceof UserAccount) {
             if (hotelId != null) {
                 UserAccount userAccount = (UserAccount) principal;
                 if (hotelId.equals(userAccount.getHotelId()))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkForGuestHelper(Authentication authentication, Long guestId) {
+        Object principal = authentication.getPrincipal();
+        if (principal instanceof UserAccount) {
+            UserAccount userAccount = (UserAccount) principal;
+            Hotel hotel = hotelService.get(userAccount.getHotelId());
+            Set<Guest> hotelGuests = hotel.getGuests();
+            for (Guest hotelGuest : hotelGuests) {
+                if (hotelGuest.getId().equals(guestId))
                     return true;
             }
         }
