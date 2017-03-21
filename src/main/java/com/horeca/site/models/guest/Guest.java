a3397811182694bba73a8e6c8a57ea6e0e791a09
Wojciech Pomykała <wojt.pomykal@gmail.com>
2017-03-21 20:41:02 +0100

    Pair (identification, nationality) must be unique in Guest model

--- src/main/java/com/horeca/site/models/guest/Guest.java
+++ src/main/java/com/horeca/site/models/guest/Guest.java
@@ -1,12 +1,10 @@
 package com.horeca.site.models.guest;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = { "identification", "nationality" }))
 public class Guest {
 
     @Id
