dd2c887a38f4797e4ffe6a38320cf91c0ef43f6e
Wojciech Pomykała <wojt.pomykal@gmail.com>
2017-03-23 18:05:58 +0100

    Add models for managing temp tokens issued to create user accounts

---
+++ src/main/java/com/horeca/site/security/models/UserAccountTempToken.java
@@ -0,0 +1,78 @@
+package com.horeca.site.security.models;
+
+import com.horeca.site.models.hotel.Hotel;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.sql.Timestamp;
+import java.util.Set;
+
+@Entity
+public class UserAccountTempToken {
+
+    @Id
+    private String token;
+
+    @ManyToOne(optional = false)
+    private Hotel hotel;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<String> roles;
+
+    @NotNull
+    private Timestamp createdAt;
+
+    @NotNull
+    private Integer validForSeconds;
+
+    UserAccountTempToken() {
+    }
+
+    public UserAccountTempToken(String token, Hotel hotel, Set<String> roles, Timestamp createdAt, Integer validForSeconds) {
+        this.token = token;
+        this.hotel = hotel;
+        this.roles = roles;
+        this.createdAt = createdAt;
+        this.validForSeconds = validForSeconds;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Hotel getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(Hotel hotel) {
+        this.hotel = hotel;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Integer getValidForSeconds() {
+        return validForSeconds;
+    }
+
+    public void setValidForSeconds(Integer validForSeconds) {
+        this.validForSeconds = validForSeconds;
+    }
+}
