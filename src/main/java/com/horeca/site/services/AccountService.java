712b361f44885d2c5413173e4977c37057de226d
Wojciech Pomykała <wojt.pomykal@gmail.com>
2017-03-23 19:41:32 +0100

    Temp token can be obtained from AccountService to create user account

--- src/main/java/com/horeca/site/services/AccountService.java
+++ src/main/java/com/horeca/site/services/AccountService.java
@@ -2,15 +2,30 @@ package com.horeca.site.services;
 
 import com.horeca.site.models.accounts.UserAccountView;
 import com.horeca.site.security.models.UserAccount;
+import com.horeca.site.security.models.UserAccountTempToken;
+import com.horeca.site.security.models.UserAccountTempTokenRequest;
+import com.horeca.site.security.models.UserAccountTempTokenResponse;
+import com.horeca.site.security.services.UserAccountTempTokenService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 @Service
 @Transactional
 public class AccountService {
+
+    @Autowired
+    private HotelService hotelService;
+
+    @Autowired
+    private UserAccountTempTokenService userAccountTempTokenService;
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public UserAccountView getCurrentUserAccount(Authentication authentication) {
@@ -21,5 +36,14 @@ public class AccountService {
         }
         else
             throw new AccessDeniedException("Access denied");
+    }
+
+    public UserAccountTempTokenResponse getTempTokenForNewUserAccount(UserAccountTempTokenRequest request) {
+        Set<String> roles = new HashSet<>(Arrays.asList(UserAccount.DEFAULT_ROLE));
+        UserAccountTempToken tempToken = userAccountTempTokenService.generateTempToken(request.getHotelId(), roles);
+
+        long currentTimeMillis = System.currentTimeMillis();
+        Long expiresIn = (tempToken.getExpiresAt().getTime() - currentTimeMillis) / 1000L;
+        return new UserAccountTempTokenResponse(tempToken.getToken(), request.getHotelId(), expiresIn.intValue());
     }
 }
