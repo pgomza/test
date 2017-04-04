f1c37c247965e6e178a5f038db43e8a2df65125f
Wojciech Pomykała <wojt.pomykal@gmail.com>
2017-04-04 16:28:41 +0200

    SimpleCorsFilter should precede any other filter

--- src/main/java/com/horeca/config/SimpleCorsFilter.java
+++ src/main/java/com/horeca/config/SimpleCorsFilter.java
@@ -1,14 +1,17 @@
 package com.horeca.config;
 
 import org.apache.log4j.Logger;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter("/*")
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class SimpleCorsFilter implements Filter {
 
     private static final Logger logger = Logger.getLogger(SimpleCorsFilter.class);
