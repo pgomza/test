287b3e7af0d8b0cdb1d5be0735e0ac595716ff26
Wojciech Pomykała <wojt.pomykal@gmail.com>
2017-04-09 17:44:21 +0200

    Add ShutdownController to facilitate application deployment

---
+++ src/main/java/com/horeca/site/controllers/ShutdownController.java
@@ -0,0 +1,36 @@
+package com.horeca.site.controllers;
+
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.endpoint.ShutdownEndpoint;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
+
+@Api(value = "hotels", hidden = true)
+@RestController
+@RequestMapping("/api/maintenance")
+public class ShutdownController {
+
+    @Autowired
+    private ShutdownEndpoint shutdownEndpoint;
+
+    @Value("${shutdownSecret}")
+    private String shutdownSecret;
+
+    @RequestMapping("/shutdown")
+    public ResponseEntity<Void> shutdown(@RequestParam(value = "secret", required = true) String secret) {
+        if (Objects.equals(secret, shutdownSecret)) {
+            shutdownEndpoint.invoke();
+            // the application context is actually closed in a different thread with some delay (by default 500 ms)
+            // so the correct status will be returned before the context closes
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
