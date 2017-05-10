8b93204b831a9f7bf272c4f793dd4c93fac12e6c
Wojciech Pomykała <wojt.pomykal@gmail.com>
2017-05-10 19:32:23 +0200

    Include 'withTestHotels' param in search methods from HotelRepository

--- src/main/java/com/horeca/site/repositories/HotelRepository.java
+++ src/main/java/com/horeca/site/repositories/HotelRepository.java
@@ -12,9 +12,10 @@ public interface HotelRepository extends PagingAndSortingRepository<Hotel, Long>
     @Query("select count(*) from Hotel")
     Long getTotalCount();
 
-    @Query("select h.id from Hotel h where lower(h.name) like %:name%")
-    List<Long> getIdsByName(@Param("name") String name);
+    @Query("select h.id from Hotel h where lower(h.name) like %:name% and h.isTestHotel = :withTestHotels")
+    List<Long> getIdsByName(@Param("name") String name, @Param("withTestHotels") Boolean withTestHotels);
 
-    @Query("select h.id as id, h.address as address from Hotel h where lower(h.address) like %:city%")
-    List<Object[]> getCandidatesByCity(@Param("city") String city);
+    @Query("select h.id as id, h.address as address from Hotel h where lower(h.address) like %:city% and " +
+            "h.isTestHotel = :withTestHotels")
+    List<Object[]> getCandidatesByCity(@Param("city") String city, @Param("withTestHotels") Boolean withTestHotels);
 }
