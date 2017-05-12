8ef69b0dc33fc4d2ee2e29886b4b986d4fc32225
Wojciech Pomykała <wojt.pomykal@gmail.com>
2017-05-12 18:56:02 +0200

    Fix 500 error on POST to ~/api/accounts/users

--- src/main/java/com/horeca/site/controllers/AccountController.java
+++ src/main/java/com/horeca/site/controllers/AccountController.java
@@ -84,6 +84,14 @@ public class AccountController {
         public ResponseMessage(String message) {
             this.message = message;
         }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
     }
 
     public static class PasswordChangeRequest {
