e692569d041791384b4fe7498591d211470713c9
Wojciech Pomykała <wojt.pomykal@gmail.com>
2017-05-16 01:28:27 +0200

    In StayRepository, add method that finds stays by hotel id

--- src/main/java/com/horeca/site/repositories/services/StayRepository.java
+++ src/main/java/com/horeca/site/repositories/services/StayRepository.java
@@ -1,7 +1,14 @@
 package com.horeca.site.repositories.services;
 
 import com.horeca.site.models.stay.Stay;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Collection;
 
 public interface StayRepository extends CrudRepository<Stay, String> {
+
+    @Query("select s.pin from Stay s where s.hotel.id = :hotelId")
+    Collection<String> findByHotelId(@Param("hotelId") Long hotelId);
 }
