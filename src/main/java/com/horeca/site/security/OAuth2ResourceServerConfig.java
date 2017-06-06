319577be012ebf5c52e35751f4b812218049a6d0
Wojciech Pomykała <wojt.pomykal@gmail.com>
2017-06-06 19:37:53 +0200

    Remove unnecessary controller

--- src/main/java/com/horeca/site/security/OAuth2ResourceServerConfig.java
+++ src/main/java/com/horeca/site/security/OAuth2ResourceServerConfig.java
@@ -42,11 +42,6 @@ public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter 
 
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-        // THIS RULE EXISTS SOLELY TO FACILITATE THE DEPLOYMENT PROCESS:
-        // allow anybody to send GET requests to the shutdown endpoint
-        // 'anybody' as far as the oauth authorization is concerned - that person has to know the secret key
-        http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/maintenance/shutdown").permitAll();
-
         // allow anybody to get info about any of the hotels (but not their guests)
         http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/hotels").permitAll();
         http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/hotels/{\\d+}").permitAll();
