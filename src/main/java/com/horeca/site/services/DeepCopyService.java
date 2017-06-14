77b289b08a9826eefa8c0f637fe285f5492c4a7d
Wojciech Pomykała <wojt.pomykal@gmail.com>
2017-06-14 19:09:06 +0200

    Make DeepCopyService standard class (not spring bean)

--- src/main/java/com/horeca/site/services/DeepCopyService.java
+++ src/main/java/com/horeca/site/services/DeepCopyService.java
@@ -2,22 +2,15 @@ package com.horeca.site.services;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
-@Service
 public class DeepCopyService {
 
     private static final Logger logger = Logger.getLogger(DeepCopyService.class);
 
-    @Autowired
-    private MappingJackson2HttpMessageConverter converter;
-
-    public synchronized <T> T deepCopy(T source) {
-        ObjectMapper mapper = converter.getObjectMapper();
+    public static <T> T copy(T source) {
+        ObjectMapper mapper = new ObjectMapper();
         Class<T> sourceClass = (Class<T>) source.getClass();
         try {
             String serialized = mapper.writeValueAsString(source);
