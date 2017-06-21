61856d996dc5e7c7eda27b992bf76856b6f48ce7
Wojciech Pomykała <wojt.pomykal@gmail.com>
2017-06-21 14:43:15 +0200

    Update WebSocketUpdatesHandler to send fake UPDATES for testing purposes

--- src/main/java/com/horeca/site/websocket/WebSocketUpdatesHandler.java
+++ src/main/java/com/horeca/site/websocket/WebSocketUpdatesHandler.java
@@ -1,5 +1,6 @@
 package com.horeca.site.websocket;
 
+import com.horeca.site.security.models.UserAccount;
 import org.apache.log4j.Logger;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.socket.CloseStatus;
@@ -7,24 +8,38 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.io.IOException;
 import java.util.Map;
-import java.util.Optional;
 
 public class WebSocketUpdatesHandler extends TextWebSocketHandler {
 
     private static Logger logger = Logger.getLogger(WebSocketUpdatesHandler.class);
 
     @Override
-    public void afterConnectionEstablished(WebSocketSession session) {
+    public void afterConnectionEstablished(WebSocketSession session) throws IOException, InterruptedException {
+        OAuth2Authentication authentication = getAuthentication(session);
+        UserAccount userAccount = (UserAccount) authentication.getPrincipal();
+        String login = userAccount.getUsername().substring(userAccount.getUsernamePrefix().length());
 
+        // for testing purposes
+        int k = 0;
+        for (int i = 0; i < 3; i++) {
+            session.sendMessage(new TextMessage("UPDATE no. " + (k++) + " for " + login));
+            Thread.sleep(10);
+            session.sendMessage(new TextMessage("UPDATE no. " + (k++) + " for " + login));
+            Thread.sleep(50);
+            session.sendMessage(new TextMessage("UPDATE no. " + (k++) + " for " + login));
+            Thread.sleep(20000);
+        }
+
+        session.close(CloseStatus.SERVICE_RESTARTED);
     }
 
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         // in this case only log the message
-        Optional<OAuth2Authentication> authentication = getAuthentication(session);
-        if (authentication.isPresent())
-            logger.debug("Received websocket message: " + message + " from " + authentication.get().getPrincipal());
+        OAuth2Authentication authentication = getAuthentication(session);
+        logger.debug("Received websocket message: " + message + " from " + authentication.getPrincipal());
     }
 
     @Override
@@ -32,9 +47,9 @@ public class WebSocketUpdatesHandler extends TextWebSocketHandler {
         session.close(CloseStatus.SERVER_ERROR);
     }
 
-    private Optional<OAuth2Authentication> getAuthentication(WebSocketSession session) {
+    private OAuth2Authentication getAuthentication(WebSocketSession session) {
         Map<String, Object> attributes = session.getAttributes();
         OAuth2Authentication authentication = (OAuth2Authentication) attributes.get("authentication");
-        return Optional.ofNullable(authentication);
+        return authentication;
     }
 }
