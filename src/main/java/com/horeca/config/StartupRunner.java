2642f398c0b7cf9cb8b4155f9fd63c93153828d2
Wojciech Pomykała <wojt.pomykal@gmail.com>
2017-07-11 18:43:34 +0200

    Check at startup if all 'active' hotels have essential data

--- src/main/java/com/horeca/config/StartupRunner.java
+++ src/main/java/com/horeca/config/StartupRunner.java
@@ -1,13 +1,20 @@
 package com.horeca.config;
 
+import com.horeca.site.repositories.UserAccountRepository;
+import com.horeca.site.security.models.UserAccount;
 import com.horeca.site.security.services.UserAccountTempTokenService;
 import com.horeca.site.services.CubilisReservationService;
+import com.horeca.site.services.HotelService;
 import com.horeca.site.websocket.WebSocketTokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 // responsible for performing various maintenance actions after
 // the startup i.e. after the application context has been loaded
@@ -24,10 +31,27 @@ public class StartupRunner {
     @Autowired
     private CubilisReservationService cubilisReservationService;
 
+    @Autowired
+    private UserAccountRepository userAccountRepository;
+
+    @Autowired
+    private HotelService hotelService;
+
     @EventListener(ContextRefreshedEvent.class)
     public void contextRefreshedEvent() {
         userAccountTempTokenService.deleteInvalidTokens();
         webSocketTokenService.deleteAll();
         cubilisReservationService.deleteOutdated();
+        ensureEnoughDataInActiveHotels();
+    }
+
+    private void ensureEnoughDataInActiveHotels() {
+        Set<UserAccount> userAccounts = new HashSet<>();
+        userAccountRepository.findAll().forEach(userAccounts::add);
+        Set<Long> activeHotelIds = userAccounts.stream()
+                .map(UserAccount::getHotelId)
+                .filter(id -> id != null && id > 0L)
+                .collect(Collectors.toSet());
+        activeHotelIds.forEach(id -> hotelService.ensureEnoughInfoAboutHotel(id));
     }
 }
