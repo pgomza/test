5ffa8cbf4c43d460be9a01f219d3f26929794db6
Wojciech Pomykała <wojt.pomykal@gmail.com>
2017-07-27 02:10:18 +0200

    Create class common for all orders

--- src/main/java/com/horeca/site/models/orders/Order.java
+++ src/main/java/com/horeca/site/models/orders/Order.java
@@ -1,10 +1,34 @@
 package com.horeca.site.models.orders;
 
-public interface Order {
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotNull;
 
-    Long getId();
-    void setId(Long id);
+@MappedSuperclass
+public abstract class Order {
 
-    OrderStatus getStatus();
-    void setStatus(OrderStatus status);
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private OrderStatus status = OrderStatus.NEW;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
 }
