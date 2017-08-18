1205cf305e761eaa6cdb466d70f22d4072edaaa9
Wojciech Pomykała <wojt.pomykal@gmail.com>
2017-08-18 17:55:13 +0200

    Use template when sending mails via EmailLabs

--- src/main/java/com/horeca/site/services/EmailSenderService.java
+++ src/main/java/com/horeca/site/services/EmailSenderService.java
@@ -14,11 +14,13 @@ import org.springframework.web.client.RestTemplate;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.io.UnsupportedEncodingException;
+import java.util.Map;
 
 @Service
 public class EmailSenderService {
 
     private static final String EMAIL_LABS_POST_EMAIL_URL = "https://api.emaillabs.net.pl/api/new_sendmail";
+    private static final String EMAIL_LABS_POST_EMAIL_TEMPLATE_URL = "https://api.emaillabs.net.pl/api/sendmail_templates";
 
     @Value("${emailLabs.active}")
     private Boolean isEmailLabsActive;
@@ -32,11 +34,32 @@ public class EmailSenderService {
     @Value("${emailLabs.smtp}")
     private String emailLabsSMTP;
 
+    @Value("${emailLabs.templateId}")
+    private String emailLabsTemplateId;
+
     @Autowired
     private JavaMailSender mailSender;
 
-    public void send(String subject, String content, String addressTo, String addressFrom, String nameFrom)
+    void send(String subject, String content, String addressTo, String addressFrom, String nameFrom)
             throws MessagingException, UnsupportedEncodingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        mimeMessage.setContent(content, "text/html");
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, false, "utf-8");
+        helper.setTo(addressTo);
+        if (nameFrom != null) {
+            helper.setFrom(addressFrom, nameFrom);
+        }
+        else {
+            helper.setFrom(addressFrom);
+        }
+        helper.setSubject(subject);
+        mailSender.send(mimeMessage);
+    }
+
+    void sendTemplate(String subject, String addressTo, String addressFrom, String nameFrom,
+                      Map<String, String> variables)
+            throws MessagingException, UnsupportedEncodingException {
+
         if (isEmailLabsActive) {
             RestTemplate restTemplate = new RestTemplate();
             restTemplate.getMessageConverters().add(new FormHttpMessageConverter());
@@ -46,27 +69,21 @@ public class EmailSenderService {
 
             MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
             params.add("to[" + addressTo + "]", addressTo);
+
+            for (Map.Entry<String, String> entry : variables.entrySet()) {
+                String name = entry.getKey();
+                String value = entry.getValue();
+
+                params.add("to[" + addressTo + "][vars][" + name + "]", value);
+            }
+
             params.add("from", addressFrom);
             params.add("from_name", nameFrom);
             params.add("smtp_account", emailLabsSMTP);
             params.add("subject", subject);
-            params.add("html", content);
+            params.add("template_id", emailLabsTemplateId);
 
-            restTemplate.postForObject(EMAIL_LABS_POST_EMAIL_URL, params, String.class);
-        }
-        else {
-            MimeMessage mimeMessage = mailSender.createMimeMessage();
-            mimeMessage.setContent(content, "text/html");
-            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, false, "utf-8");
-            helper.setTo(addressTo);
-            if (nameFrom != null) {
-                helper.setFrom(addressFrom, nameFrom);
-            }
-            else {
-                helper.setFrom(addressFrom);
-            }
-            helper.setSubject(subject);
-            mailSender.send(mimeMessage);
+            restTemplate.postForObject(EMAIL_LABS_POST_EMAIL_TEMPLATE_URL, params, String.class);
         }
     }
 }
