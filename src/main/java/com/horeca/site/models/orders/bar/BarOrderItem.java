b7bf3572ccc3ae7f8cd69fb175c2cf717e8450a6
Wojciech Pomykała <wojt.pomykal@gmail.com>
2017-08-31 14:40:03 +0200

    Fix cascade type in orders to referenced items

--- src/main/java/com/horeca/site/models/orders/bar/BarOrderItem.java
+++ src/main/java/com/horeca/site/models/orders/bar/BarOrderItem.java
@@ -15,7 +15,7 @@ public class BarOrderItem {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn
     private BarItem item;
 
