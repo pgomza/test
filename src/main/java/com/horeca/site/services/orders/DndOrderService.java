eaa4603611a6b13710df9a33343904bc40ca5a24
Wojciech Pomykała <wojt.pomykal@gmail.com>
2017-08-31 19:25:38 +0200

    Publish order event when updating dnd status

--- src/main/java/com/horeca/site/services/orders/DndOrderService.java
+++ src/main/java/com/horeca/site/services/orders/DndOrderService.java
@@ -1,10 +1,12 @@
 package com.horeca.site.services.orders;
 
+import com.horeca.site.models.notifications.DndOrderEvent;
 import com.horeca.site.models.orders.Orders;
 import com.horeca.site.models.orders.dnd.DndOrder;
 import com.horeca.site.models.orders.dnd.DndOrderUPDATE;
 import com.horeca.site.repositories.orders.DndOrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +20,9 @@ public class DndOrderService {
     @Autowired
     private DndOrderRepository repository;
 
+    @Autowired
+    private ApplicationEventPublisher eventPublisher;
+
     public DndOrder get(String stayPin) {
         Orders orders = ordersService.get(stayPin);
         return orders.getDnd();
@@ -25,7 +30,12 @@ public class DndOrderService {
 
     public DndOrder update(String stayPin, DndOrderUPDATE updated) {
         DndOrder oldOne = get(stayPin);
-        oldOne.setStatus(updated.getStatus());
+
+        if (oldOne.getStatus() != updated.getStatus()) {
+            oldOne.setStatus(updated.getStatus());
+            eventPublisher.publishEvent(new DndOrderEvent(this, stayPin));
+        }
+
         return repository.save(oldOne);
     }
 }
