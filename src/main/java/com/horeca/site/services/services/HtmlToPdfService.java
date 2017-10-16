0ac14b43b8d311b5809892b790098e77012ca053
Wojciech Pomykała <wojt.pomykal@gmail.com>
2017-10-16 13:57:51 +0200

    Pass desired page width and height to html-to-pdf service

--- src/main/java/com/horeca/site/services/services/HtmlToPdfService.java
+++ src/main/java/com/horeca/site/services/services/HtmlToPdfService.java
@@ -15,13 +15,14 @@ public class HtmlToPdfService {
 
     private static final String BASE_URI = "http://example.com";
 
-    public synchronized ByteArrayOutputStream convert(InputStream htmlInputStream) throws Exception {
+    public synchronized ByteArrayOutputStream convert(InputStream htmlInputStream, float width, float height)
+            throws Exception {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
         PdfRendererBuilder builder = new PdfRendererBuilder();
         Document doc = html5ParseDocument(htmlInputStream);
         builder.withW3cDocument(doc, BASE_URI);
-        builder.useDefaultPageSize(11.69f, 8.27f, PdfRendererBuilder.PageSizeUnits.INCHES);
+        builder.useDefaultPageSize(width, height, PdfRendererBuilder.PageSizeUnits.INCHES);
         builder.toStream(outputStream);
         builder.run();
 
