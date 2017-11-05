88dd5fce3cfdbcbac37172fc747f84913bde0fc9
Wojciech Pomykała <wojt.pomykal@gmail.com>
2017-11-05 22:29:10 +0100

    Force client to renew token after changing password

--- src/main/java/com/horeca/site/security/services/UserAccountService.java
+++ src/main/java/com/horeca/site/security/services/UserAccountService.java
@@ -90,6 +90,10 @@ public class UserAccountService extends AbstractAccountService<UserAccount> {
         UserAccount userAccount = get(login);
         userAccount.setPassword(hashedPassword);
         save(userAccount);
+
+        Collection<OAuth2AccessToken> tokens = tokenStore
+                .findTokensByClientIdAndUserName(PANEL_CLIENT_ID, userAccount.getUsername());
+        tokens.forEach(tokenStore::removeAccessToken);
     }
 
     public void disableAllInHotel(Long hotelId) {
