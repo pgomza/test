c2f0a447dbe7669b5593eccb3ec929694f49dd47
Wojciech Pomykała <wojt.pomykal@gmail.com>
2017-11-06 15:29:58 +0100

    Create service for root account

---
+++ src/main/java/com/horeca/site/security/services/RootAccountService.java
@@ -0,0 +1,40 @@
+package com.horeca.site.security.services;
+
+import com.horeca.site.security.models.AbstractAccount;
+import com.horeca.site.security.models.RootAccount;
+import com.horeca.site.security.repositories.RootAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class RootAccountService extends AbstractAccountService<RootAccount> {
+
+    @Autowired
+    private RootAccountRepository repository;
+
+    @Autowired
+    private LoginService loginService;
+
+    @Override
+    protected CrudRepository<RootAccount, String> getRepository() {
+        return repository;
+    }
+
+    @Override
+    public boolean exists(String login) {
+        return loginService.exists(AbstractAccount.PANEL_CLIENT_USERNAME_PREFIX + login);
+    }
+
+    @Override
+    public RootAccount get(String login) {
+        return getRepository().findOne(AbstractAccount.PANEL_CLIENT_USERNAME_PREFIX + login);
+    }
+
+    @Override
+    public void delete(String login) {
+        getRepository().delete(AbstractAccount.PANEL_CLIENT_USERNAME_PREFIX + login);
+    }
+}
