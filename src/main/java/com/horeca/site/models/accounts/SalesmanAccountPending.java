b08cfea0afb04b24a881f4e106795ff37ae537f7
Wojciech Pomykała <wojt.pomykal@gmail.com>
2017-11-14 23:13:24 +0100

    Create model of pending salesman account

---
+++ src/main/java/com/horeca/site/models/accounts/SalesmanAccountPending.java
@@ -0,0 +1,69 @@
+package com.horeca.site.models.accounts;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.joda.time.Instant;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class SalesmanAccountPending {
+
+    @Id
+    @Email
+    private String email;
+
+    @NotEmpty
+    private String password;
+
+    @NotEmpty
+    private String secret;
+
+    @NotNull
+    @LastModifiedDate
+    private Long lastModifiedAt;
+
+    SalesmanAccountPending() {}
+
+    public SalesmanAccountPending(String email, String password, String secret) {
+        this.email = email;
+        this.password = password;
+        this.secret = secret;
+        this.lastModifiedAt = Instant.now().getMillis();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+
+    public Long getLastModifiedAt() {
+        return lastModifiedAt;
+    }
+
+    public void setLastModifiedAt(Long lastModifiedAt) {
+        this.lastModifiedAt = lastModifiedAt;
+    }
+}
