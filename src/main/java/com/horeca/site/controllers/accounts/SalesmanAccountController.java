ad7d5892fade8e4667860c8995361664cad7d468
Wojciech Pomykała <wojt.pomykal@gmail.com>
2017-11-15 14:51:17 +0100

    Expose endpoint for activating salesman account

--- src/main/java/com/horeca/site/controllers/accounts/SalesmanAccountController.java
+++ src/main/java/com/horeca/site/controllers/accounts/SalesmanAccountController.java
@@ -1,17 +1,23 @@
 package com.horeca.site.controllers.accounts;
 
-import com.horeca.site.models.accounts.SalesmanAccountPOST;
+import com.horeca.site.models.accounts.AccountPOST;
+import com.horeca.site.models.accounts.AccountPending;
 import com.horeca.site.models.accounts.SalesmanAccountView;
 import com.horeca.site.security.models.SalesmanAccount;
 import com.horeca.site.security.services.SalesmanAccountService;
+import com.horeca.site.services.accounts.AccountPendingService;
+import com.horeca.site.services.accounts.SalesmanAccountPendingService;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import javax.mail.MessagingException;
 import javax.validation.Valid;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,16 +27,38 @@ import java.util.stream.Collectors;
 public class SalesmanAccountController {
 
     @Autowired
-    private SalesmanAccountService service;
+    private SalesmanAccountService accountService;
+
+    @Autowired
+    private SalesmanAccountPendingService pendingService;
 
     @RequestMapping(value = "/salesmen", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<SalesmanAccountView> getAll() {
-        return service.getAll().stream().map(SalesmanAccount::toView).collect(Collectors.toList());
+        return accountService.getAll().stream().map(SalesmanAccount::toView).collect(Collectors.toList());
     }
 
+    @Transactional
     @RequestMapping(value = "/salesmen", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public SalesmanAccountView create(@RequestBody @Valid SalesmanAccountPOST accountPOST) {
-        return service.create(accountPOST.getLogin(), accountPOST.getPassword()).toView();
+    public AccountPendingService.ResponseMessage addPending(@RequestBody @Valid AccountPOST accountPOST) {
+        AccountPending pending = pendingService.add(accountPOST);
+        try {
+            pendingService.sendActivationEmail(pending);
+        } catch (UnsupportedEncodingException | MessagingException e) {
+            throw new RuntimeException("There was a problem while trying to send an email to " + pending.getEmail(), e);
+        }
+        return AccountPendingService.prepareResponseMessage(pending.getEmail());
+    }
+
+    @RequestMapping(value = "/salesmen/activation", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+    public String activatePending(@RequestParam(value = "secret") String secret) {
+        String outcome = "Activation successful";
+        try {
+            pendingService.activate(secret);
+        } catch (RuntimeException ex) {
+            outcome = "Activation failed";
+        }
+
+        return pendingService.prepareRedirectPage(outcome);
     }
 
     @RequestMapping(value = "/salesmen/current", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -40,12 +68,12 @@ public class SalesmanAccountController {
 
     @RequestMapping(value = "/salesmen/{login}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public SalesmanAccountView get(@PathVariable("login") String login) {
-        return service.get(login).toView();
+        return accountService.get(login).toView();
     }
 
     @RequestMapping(value = "/salesmen/{login}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
     public void delete(@PathVariable("login") String login) {
-        service.delete(login);
+        accountService.delete(login);
     }
 
     private SalesmanAccount authenticationToSalesmanAccount(Authentication authentication) {
