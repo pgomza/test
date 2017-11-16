d59782de744d6182642d8b2d103fcf4e20f2940c
Wojciech Pomykała <wojt.pomykal@gmail.com>
2017-11-16 01:08:33 +0100

    Manage profile data in base account service

--- src/main/java/com/horeca/site/security/services/AbstractAccountService.java
+++ src/main/java/com/horeca/site/security/services/AbstractAccountService.java
@@ -5,6 +5,8 @@ import com.horeca.site.security.models.AbstractAccount;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Map;
+
 public abstract class AbstractAccountService<T extends AbstractAccount> {
 
     final static String PASSWORD_REGEX = "[^\\s]{5,}";
@@ -37,6 +39,17 @@ public abstract class AbstractAccountService<T extends AbstractAccount> {
 
     public T save(T account) {
         return getRepository().save(account);
+    }
+
+    public Map<String, String> getProfileData(String login) {
+        return get(login).getProfileData();
+    }
+
+    public Map<String, String> updateProfileData(String login, Map<String, String> updated) {
+        T account = get(login);
+        account.getProfileData().clear();
+        account.getProfileData().putAll(updated);
+        return save(account).getProfileData();
     }
 
     void disable(T account) {
