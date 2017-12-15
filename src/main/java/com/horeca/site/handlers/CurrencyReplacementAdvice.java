cfdf6717e0f42449f47f34a63c892520e2e20d38
Wojciech Pomykała <wojt.pomykal@gmail.com>
2017-12-15 19:24:49 +0100

    Create advice that replaces currency in annotated controllers

--- src/main/java/com/horeca/site/handlers/CurrencyReplacementAdvice.java
+++ src/main/java/com/horeca/site/handlers/CurrencyReplacementAdvice.java
@@ -2,18 +2,23 @@ package com.horeca.site.handlers;
 
 import com.horeca.site.repositories.services.StayRepository;
 import com.horeca.site.services.CurrencyReplacementService;
+import org.apache.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
 
 @Aspect
 @Component
 public class CurrencyReplacementAdvice extends HotelAdvice {
 
+    private final Logger logger = Logger.getLogger(getClass().getName());
     private final CurrencyReplacementService service;
 
     @Autowired
@@ -25,8 +30,15 @@ public class CurrencyReplacementAdvice extends HotelAdvice {
     @Around(value = "@annotation(ReplaceCurrency)")
     public Object replace(ProceedingJoinPoint joinPoint) throws Throwable {
         Object originalObject = joinPoint.proceed();
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
+                .getRequest();
 
         Optional<Long> hotelIdOpt = extractHotelId(joinPoint);
-        service.replace(originalObject, ho)
+        if (!hotelIdOpt.isPresent()) {
+            logger.error("Couldn't extract hotelId when processing " + request.getRequestURI());
+            return originalObject;
+        }
+
+        return service.replace(originalObject, hotelIdOpt.get());
     }
 }
