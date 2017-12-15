7203710aa9fff0b461cd2761c2493849f2de676d
Wojciech Pomykała <wojt.pomykal@gmail.com>
2017-12-15 19:06:48 +0100

    Move extraction of hotel id to base class

---
+++ src/main/java/com/horeca/site/handlers/HotelAdvice.java
@@ -0,0 +1,43 @@
+package com.horeca.site.handlers;
+
+import com.horeca.site.repositories.services.StayRepository;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.reflect.MethodSignature;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.Optional;
+
+public abstract class HotelAdvice {
+
+    protected final StayRepository stayRepository;
+
+    public HotelAdvice(StayRepository stayRepository) {
+        this.stayRepository = stayRepository;
+    }
+
+    public Optional<Long> extractHotelId(ProceedingJoinPoint joinPoint) {
+        Object[] methodArgs = joinPoint.getArgs();
+        Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
+        Parameter[] parameters = method.getParameters();
+
+        boolean foundHotelId = false;
+        Long hotelId = null;
+        int i = 0;
+        while (i < parameters.length && !foundHotelId) {
+            Parameter param = parameters[i];
+            if (param.getAnnotation(HotelId.class) != null) {
+                hotelId = (Long) methodArgs[i];
+                foundHotelId = true;
+            }
+            else if (param.getAnnotation(StayPin.class) != null) {
+                String pin = (String) methodArgs[i];
+                hotelId = stayRepository.getHotelIdOfStay(pin);
+                foundHotelId = true;
+            }
+            i++;
+        }
+
+        return Optional.ofNullable(hotelId);
+    }
+}
