76639523a4ff47fed742f75233463eee0ee85a3b
Wojciech Pomykała <wojt.pomykal@gmail.com>
2018-02-07 16:17:49 +0100

    Create subscription model

---
+++ src/main/java/com/horeca/site/models/hotel/subscription/Subscription.java
@@ -0,0 +1,59 @@
+package com.horeca.site.models.hotel.subscription;
+
+import com.horeca.site.models.hotel.Hotel;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Entity
+public class Subscription {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "hotel_id")
+    private Hotel hotel;
+
+    @NotNull
+    private Boolean trialEligible;
+
+    @NotNull
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+    @JoinColumn(name = "subscription_id")
+    private List<SubscriptionEvent> history;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Hotel getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(Hotel hotel) {
+        this.hotel = hotel;
+    }
+
+    public Boolean getTrialEligible() {
+        return trialEligible;
+    }
+
+    public void setTrialEligible(Boolean trialEligible) {
+        this.trialEligible = trialEligible;
+    }
+
+    public List<SubscriptionEvent> getHistory() {
+        return history;
+    }
+
+    public void setHistory(List<SubscriptionEvent> history) {
+        this.history = history;
+    }
+}
---
+++ src/main/java/com/horeca/site/models/hotel/subscription/SubscriptionEvent.java
@@ -0,0 +1,57 @@
+package com.horeca.site.models.hotel.subscription;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.sql.Timestamp;
+
+@Entity
+public class SubscriptionEvent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdAt;
+
+    @NotNull
+    private Integer level;
+
+    @NotNull
+    private Integer validityPeriod;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    public Integer getValidityPeriod() {
+        return validityPeriod;
+    }
+
+    public void setValidityPeriod(Integer validityPeriod) {
+        this.validityPeriod = validityPeriod;
+    }
+}
