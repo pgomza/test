446e051ddd0456dedeeb75c61a3100baadd27df7
Wojciech Pomykała <wojt.pomykal@gmail.com>
2018-02-07 16:20:51 +0100

    Create view of subscription

---
+++ src/main/java/com/horeca/site/models/hotel/subscription/SubscriptionView.java
@@ -0,0 +1,39 @@
+package com.horeca.site.models.hotel.subscription;
+
+import javax.validation.constraints.NotNull;
+import java.sql.Timestamp;
+
+public class SubscriptionView {
+
+    @NotNull
+    private Integer currentLevel;
+
+    private Timestamp expiresAt;
+
+    @NotNull
+    private Boolean trialEligible;
+
+    public Integer getCurrentLevel() {
+        return currentLevel;
+    }
+
+    public void setCurrentLevel(Integer currentLevel) {
+        this.currentLevel = currentLevel;
+    }
+
+    public Timestamp getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(Timestamp expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+
+    public Boolean getTrialEligible() {
+        return trialEligible;
+    }
+
+    public void setTrialEligible(Boolean trialEligible) {
+        this.trialEligible = trialEligible;
+    }
+}
